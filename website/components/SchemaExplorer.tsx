5352e1fde085a434cbc5f39ff12e85c0078c7aa9
Rishichandra Wawhal <rishi@hasura.io>
2022-11-08 19:43:28 +0530

    add a placeholder comment

--- website/components/SchemaExplorer.tsx
+++ website/components/SchemaExplorer.tsx
@@ -28,9 +28,9 @@ export const SchemaExplorer: React.VFC<{ schema: any}> = (props) => {
       <div className="mb-2">
         <p>Explore the GraphQL schema</p>
       </div>
-      <GraphiQLProvider fetcher={fetcher}>
+      <GraphiQLProvider fetcher={fetcher} query="# Try writing a GraphQL query across the generated GraphQL schema">
         <div className="graphiql-container h-96 border border border-gray-200 rounded" style={{maxHeight: '100%', height: '400px'}}>
-          <QueryEditor />
+          <QueryEditor/>
         </div>
       </GraphiQLProvider>
       </div>
