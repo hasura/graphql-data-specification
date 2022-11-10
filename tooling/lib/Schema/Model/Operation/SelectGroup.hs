205e5fa6c31d462679d97b15acbfdb6b6d2dffbc
Vamshi Surabhi <vamshi@vsurabhi.in>
2022-11-10 16:14:03 +0530

    tooling: add "having" argument to "groupby" api

--- tooling/lib/Schema/Model/Operation/SelectGroup.hs
+++ tooling/lib/Schema/Model/Operation/SelectGroup.hs
@@ -20,15 +20,19 @@ definition fieldNamePrefix model = do
           GraphQL.TypeNamed (GraphQL.Nullability False) selectionSetTypeName
   arguments <- ListArguments.arguments model.name
   selectableFieldTypeName <- getTypeName $ TGRSelectableField model.name
+  aggregateBooleanExpressionTypeName <- getTypeName $ TGRBooleanExpressionAggregate model.name
   let groupByField =
         mkInputValueDefinition (mkFieldName "group_by") $
           GraphQL.TypeList (GraphQL.Nullability True) $
             GraphQL.TypeNamed (GraphQL.Nullability False) selectableFieldTypeName
+      havingField =
+        mkInputValueDefinition (mkFieldName "having") $
+          GraphQL.TypeNamed (GraphQL.Nullability True) aggregateBooleanExpressionTypeName
   pure $
     GraphQL.FieldDefinition
       { _fldDescription = Nothing,
         _fldName = fieldName,
-        _fldArgumentsDefinition = groupByField : arguments,
+        _fldArgumentsDefinition = groupByField : havingField : arguments,
         _fldType = fieldType,
         _fldDirectives = []
       }
