9dda8b04a7fd208913661588de22a32467e73a2a
Rishichandra Wawhal <rishi@hasura.io>
2022-11-14 11:35:59 +0530

    playground: add ga script (#12)

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S7LTE28HRP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-S7LTE28HRP');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
